b803ee3a970f2409db40c3d14bc6d0d8ebb3cd4f
Kiarash Zamanifar <kiarash.zam@gmail.com>
2015-09-18 14:03:32 +1000

    initialized befor refactoring class names

---
+++ src/main/java/com/telflow/WordCounter.java
@@ -0,0 +1,195 @@
+package com.telflow;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Stream;
+
+/**
+ * The main class for the console application to read a text file and count the number of words
+ * that meet the given condition on command line.
+ *
+ * Usage: java -cp [packaged jar file] com.telflow.WordCounter startsWith=m,M longerThan=5 [filename]
+ *
+ * The value for <strong>startsWith</strong> specifies the restriction on valid starting characters of the words
+ * that need to be counted.
+ *
+ * The value for <strong>longerThan</strong> shows the the minimum length of all words that need to be counted.
+ *
+ */
+public class WordCounter {
+
+    public final static String STARTS_WITH = "startsWith";
+    public final static String LONGER_THAN = "longerThan";
+    public final static String FILENAME = "filename";
+    public final static String STARTS_WITH_REGEX = "starts[wW]ith=[a-zA-Z](,[a-zA-Z])*";
+    public final static String LONGER_THAN_REGEX = "longer[tT]han=[1-9][0-9]*";
+
+
+    /**
+     * Entry point - the main method of application
+     *
+     * @param args - string array containing command line arguments
+     */
+    public static void main( String[] args ) {
+
+        ResourceBundle bundle = ResourceBundle.getBundle("messages");
+        Console console = System.console();
+        Map<String,String> commandArguments;
+
+
+        if (args.length != 3) {
+            console.printf("%s %n", bundle.getString("line"));
+            console.printf("%s %n", bundle.getString("input.error.message"));
+            console.printf("%s %n", bundle.getString("line"));
+            console.printf("%s %n %n", bundle.getString("usage.message"));
+            console.printf("%s %n %n", bundle.getString("usage.example"));
+            System.exit(-1);
+        }
+        try {
+            // validate input command line arguments
+            commandArguments = readAndValidateCommandParameters(args, bundle);
+            int lengthLimit = Integer.parseInt(commandArguments.get(LONGER_THAN));
+            String validCharacters = commandArguments.get(STARTS_WITH);
+
+            // Convert text file into Stream
+            Stream<String> wordsStream = readWordsIntoStream(commandArguments.get(FILENAME));
+
+            // initialize list of words
+            BlockingQueue lengthRestrictedList = new LinkedBlockingDeque<>();
+
+            // initialize number of words counter
+            AtomicLong atomicCounter = new AtomicLong(0);
+
+            // Process the stream
+            processStream(wordsStream, lengthRestrictedList, atomicCounter, lengthLimit, validCharacters);
+
+            console.printf("%s %s %n", MessageFormat.format(bundle.getString("counter.message"),validCharacters), atomicCounter.toString());
+            console.printf("%s %n", MessageFormat.format(bundle.getString("list.counted.message"), lengthLimit, lengthRestrictedList.size()));
+            console.printf("%s %s %n", MessageFormat.format(bundle.getString("list.message"),lengthLimit),
+                    lengthRestrictedList.size() > 0 ? lengthRestrictedList.toString() :
+                            MessageFormat.format(bundle.getString("list.empty.message"),lengthLimit));
+        }
+        catch (IllegalArgumentException e) {
+            console.printf("%s %n", e.getMessage());
+        }
+    }
+
+    /**
+     * This method calls functional methods as part of stream processing.
+     * Further methods can be implemented and added here if more conditions
+     * need to be checked/implemented.
+     *
+     * @param wordsStream
+     * @param lengthRestrictedList
+     * @param startCharRestrictedCounter
+     * @param lengthLimit
+     * @param validCharacters
+     */
+    protected static void processStream(Stream<String> wordsStream, Queue<String> lengthRestrictedList, AtomicLong startCharRestrictedCounter, int lengthLimit, String validCharacters) {
+        wordsStream.parallel().forEach(s -> {
+            add2List(s, lengthLimit, lengthRestrictedList);
+            increment(s, validCharacters, startCharRestrictedCounter);
+        });
+    }
+
+    /**
+     * This methods will be used as a Consumer inside the Stream processing to count the
+     * number of words that start with the given list of characters.
+     *
+     * As the Stream may be processed in parallel on different cores, atomic number used
+     * to be thread safe.
+     *
+     * @param s - input string to check
+     * @param validCharacters
+     * @param atomicLong - word counter
+     */
+    protected static void increment(String s, String validCharacters, AtomicLong atomicLong) {
+        if (checkStartChar(s,validCharacters)) {
+            atomicLong.incrementAndGet();
+        }
+    }
+
+    /**
+     * This methods will be used as a Consumer inside the Stream processing to add the
+     * words to the given list if their length are longer than given number.
+     *
+     * @param s
+     * @param lengthLimit
+     * @param lengthRestrictedList
+     */
+    protected static void add2List(String s, int lengthLimit, Queue<String> lengthRestrictedList) {
+        if (s.length() > lengthLimit) {
+            lengthRestrictedList.add(s);
+        }
+    }
+
+    /**
+     * Utility method to check the given string starts with valid characters.
+     *
+     * @param inputString
+     * @param validStartingChars
+     * @return boolean indicating the validity of input string.
+     */
+    protected static boolean checkStartChar(String inputString, String validStartingChars) {
+        for (String c: validStartingChars.split(",")) {
+            if (inputString.startsWith(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * This method checks the validity of command line arguments passed in as a string array and
+     * throws IllegalArgumentException of not all arguments are valid.
+     *
+     * @param args - String array containing command line arguments.
+     * @return Map<String, String> - a map containing validated arguments.
+     * @throws IllegalArgumentException
+     */
+    protected static Map<String,String> readAndValidateCommandParameters(String[] args, ResourceBundle bundle) throws IllegalArgumentException {
+        Map<String, String> cmdArgs = new HashMap<>(3);
+        int validated = 0;
+        for (String str: args) { // starting character constraint
+            if (str.matches(STARTS_WITH_REGEX)) {
+                cmdArgs.put(STARTS_WITH,str.substring(str.indexOf("=")+1));
+                validated = validated | (1 << 0);
+            }
+            else if (str.matches(LONGER_THAN_REGEX)) { // length limit parameter
+                cmdArgs.put(LONGER_THAN,str.substring(str.indexOf("=")+1));
+                validated = validated | (1 << 1);
+            }
+            else { // input filename
+                cmdArgs.put(FILENAME,str);
+                validated = validated | (1 << 2);
+            }
+        }
+        if (validated != 7) {
+            throw new IllegalArgumentException(
+                    bundle.getString("format.error.message"));
+        }
+        return cmdArgs;
+    }
+
+    /**
+     * Utility method to read a text file and return the words as a Stream.
+     * @param filename
+     * @return Stream of words
+     */
+    protected static Stream<String> readWordsIntoStream(String filename) {
+        List<String> words = new LinkedList<>();
+        try (Stream<String> lines = Files.lines(Paths.get(filename))) {
+            lines.parallel().forEach(s -> words.addAll(Arrays.asList(s.split("\\W+"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words.stream();
+    }
+}
---
+++ src/test/java/com/telflow/WordCounterTest.java
@@ -0,0 +1,178 @@
+package com.telflow;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * A Test class that test methods of WordCounter class using JUnit.
+ *
+ * @version $Id$
+ */
+public class WordCounterTest {
+
+    ResourceBundle bundle = ResourceBundle.getBundle("messages");
+
+    /**
+     * Tests WordCounter.readCommandParameter(String[] args) method when input parameters are valid.
+     */
+    @Test
+    public void readCommandParameters_validInputTest() {
+
+        String[] validArgs = {"startswith=m,M", "longerThan=3", "./words.txt"};
+        Map<String,String> expected = new HashMap<>();
+        expected.put(WordCounter.STARTS_WITH,"m,M");
+        expected.put(WordCounter.LONGER_THAN, "3");
+        expected.put(WordCounter.FILENAME, "./words.txt");
+
+        Map<String,String> actual = WordCounter.readAndValidateCommandParameters(validArgs, bundle);
+        assertEquals("Method readAndValidateCommandParameters failed to return valid Map", actual, expected);
+    }
+
+    /**
+     * Tests WordCounter.readCommandParameter(String[] args) method when input parameters
+     * are NOT valid strings.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void readCommandParameters_invalidInputsTest() {
+
+        String[] invalidArgs = {"startswith m,M", "longerthen 5", "something"};
+
+        // expecting exception
+        WordCounter.readAndValidateCommandParameters(invalidArgs, bundle);
+    }
+
+
+    /**
+     * Tests WordCounter.readCommandParameter(String[] args) method when NOT all
+     * input parameters are provided.
+     */
+    @Test(expected=IllegalArgumentException.class)
+    public void readCommandParameters_invalidNumberOfParametersTest() {
+
+        String[] invalidNumberOfArgs = {"startswith=m,M"};
+
+        // expecting exception
+        WordCounter.readAndValidateCommandParameters(invalidNumberOfArgs, bundle);
+    }
+
+    /**
+     * Tests WordCounter.readWordsIntoStreamTest(String filename) method for a valid text file
+     */
+    @Test
+    public void readWordsIntoStreamTest() {
+        StringBuilder stringBuilder = new StringBuilder(Paths.get("").toAbsolutePath().toString());
+        stringBuilder.append(File.separator);
+        stringBuilder.append("src");
+        stringBuilder.append(File.separator);
+        stringBuilder.append("test");
+        stringBuilder.append(File.separator);
+        stringBuilder.append("resources");
+        stringBuilder.append(File.separator);
+        stringBuilder.append("words.txt");
+        String filename = stringBuilder.toString();
+
+        String[] expected = {"You","may","come","from","an","object",
+                "oriented","or","a","more","diverse","development","background",
+                "But","you","will","have","an","agnostic","mindset","in","terms",
+                "of","technology","and","development","languages","and","keen","to",
+                "gain","broader","development","experience"};
+        Arrays.sort(expected);
+
+        Stream<String> actual = WordCounter.readWordsIntoStream(filename);
+        List<String> actualList = actual.collect(Collectors.toList());
+        String[] actualArray = new String[actualList.size()];
+        actualList.toArray(actualArray);
+        Arrays.sort(actualArray);
+        assertTrue("The readWordsIntoStreamTest methods returns invalid stream!", Arrays.equals(expected, actualArray));
+    }
+
+
+    /**
+     * Tests WordCounter.checkStartChar(String word, String startingChars) method for a
+     * valid condition.
+     */
+    @Test
+    public void checkStartChar_trueTest() {
+        String word = "Melbourne";
+        String startingChars = "m,M";
+        assertTrue("The method checkStartChar failed to find m/M as starting character!",
+                WordCounter.checkStartChar(word, startingChars));
+    }
+
+    /**
+     * Tests WordCounter.checkStartChar(String word, String startingChars) method for an
+     * invalid condition.
+     */
+    @Test
+    public void checkStartChar_falseTest() {
+        String word = "Sydney";
+        String startingChars = "m,M";
+        assertFalse("The method checkStartChar failed to find m/M as starting character!",
+                WordCounter.checkStartChar(word, startingChars));
+    }
+
+    /**
+     * Tests WordCounter.increment(String inputString, String validCharacters, AtomicLong atomicLong) method
+     * to check the incrementation.
+     */
+    @Test
+    public void incrementTest() {
+        String inputString = "Atomic";
+        String validCharacters = "a,A";
+        AtomicLong atomicLong = new AtomicLong(0);
+
+        WordCounter.increment(inputString, validCharacters, atomicLong);
+
+        assertEquals("The increment method failed to do its job!!", 1, atomicLong.intValue());
+    }
+
+    /**
+     * This method is for testing the processStream which handles the whole business logic.
+     *
+     */
+    @Test
+    public void processStream() {
+
+        Stream<String> inputStream = Stream.of("You","may","come","from","an","Object","oriented");
+        String[] expectedWords = {"come", "from", "Object", "oriented"};
+        Arrays.sort(expectedWords);
+        Queue<String> actualQueue = new LinkedBlockingDeque<>();
+        AtomicLong actualWordCounter = new AtomicLong(0);
+        String chars2StartWith = "o,O";
+        int lengthLimit = 3;
+
+        WordCounter.processStream(inputStream, actualQueue, actualWordCounter, lengthLimit, chars2StartWith);
+
+        // convert actualQueue to array to be able to compare it with expected words list.
+        String[] actualWords = actualQueue.toArray(new String[actualQueue.size()]);
+        Arrays.sort(actualWords);
+        assertTrue("Not a valid list of words returned!", Arrays.equals(actualWords, expectedWords));
+        assertEquals("Not a valid number of words returned!", 2, actualWordCounter.intValue());
+
+    }
+
+    /**
+     * Testing WordCounter.add2List(String s, int lengthLimit, List<String> list)
+     *
+     */
+    @Test
+    public void add2List() {
+        String inputString = "increment";
+        Queue<String> queue = new LinkedBlockingDeque<>();
+
+        WordCounter.add2List(inputString, inputString.length() - 1, queue);
+
+        assertTrue("The add2List method modified the list incorrectly!",
+
+                queue.contains(inputString) && queue.size() == 1);
+    }
+}
